056528fe459a3ee00bb621f2879c5c671bbefe38
francescopanico <frpanico96@gmail.com>
2024-03-27 23:03:59 +0100

    Revert "[SWIFT] Swift Fundamentals - first commit"
    This reverts commit 374f3df9607c820e29cb39b60b3c95b62295830a.

--- SwiftFundamentals/ViewController.swift
+++ SwiftFundamentals/ViewController.swift
@@ -8,26 +8,10 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var mainLabel: UILabel!
-    
-    @IBAction func onPressBtn(_ sender: Any) {
-        print("The button was pressed")
-        mainLabel.text = "This app rocks!"
-    }
-    @IBOutlet var clickMeBtn: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        navigationController?.title = "Debugging"
-        
-        var names = ["Tammy", "Cole"]
-        names.removeFirst()
-        names.removeFirst()
-        //names.removeFirst()
-        
-        clickMeBtn.tintColor = .red
-        
         // Do any additional setup after loading the view.
-        //let x = 4
     }
 
 
