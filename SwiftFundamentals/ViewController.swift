bb70faec49d267218771f9b7c037cdfefd9c1d51
francescopanico <frpanico96@gmail.com>
2024-04-01 00:08:10 +0200

    [SWIFT] Light App

--- SwiftFundamentals/ViewController.swift
+++ SwiftFundamentals/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var lightOn = true
+    //@IBOutlet var lightButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
 
-
+    fileprivate func updateUI() {
+        //let btnText: String = lightOn ? "Off" : "On"
+        
+        view.backgroundColor = lightOn ? .white : .black
+        //lightButton.setTitle(btnText, for: .normal)
+        
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        updateUI()
+    }
+    
 }
 
