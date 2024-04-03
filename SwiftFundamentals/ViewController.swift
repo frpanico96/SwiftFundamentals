a8135b55b0a37a60134ccd1f5323c99efe485e19
francescopanico <frpanico96@gmail.com>
2024-04-03 23:40:54 +0200

    [SWIFT] Strings

--- SwiftFundamentals/ViewController.swift
+++ SwiftFundamentals/ViewController.swift
@@ -12,6 +12,48 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+
+        
+        /* String interpolation */
+        let name = "Sophie";
+        let age = 30;
+        
+        print("\(name) is \(age) years old");
+        
+        /* String size */
+        
+        print("Size of string \(name) is: \(name.count)");
+        
+        /* Strings operations */
+        
+        let greeting: String = "Hello, world!"
+        
+        print(greeting.hasSuffix("world!"));
+        print(greeting.hasSuffix("World!"));
+        print(greeting.hasPrefix("Hello"));
+        if greeting.contains("lo, wor"){
+            print("Greetings")
+        }
+
+        let someChar: Character = "e";
+        switch someChar{
+        case "a", "e", "i":
+            print("a e i");
+        case "o", "u":
+            print("o u");
+        default:
+            print("consonant");
+        }
+        
+        /*
+         Unicode character are treated as single character of length 1
+         In reality those unicode character are composed of more (invisble) character
+         e.g. "👩🏽‍🎓" is composed of four character!
+         */
+        let cow = "🐮";
+        print("size of \(cow) is \(cow.count)");
+        
     }
 
 
