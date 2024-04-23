8c2e81cef86ced623070578ac9111447ad02f05a
francescopanico <frpanico96@gmail.com>
2024-04-24 00:22:49 +0200

    [SWIFT] Guided Project - Apple Pie

--- SwiftFundamentals/Game.swift
+++ SwiftFundamentals/Game.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    var guessedLetters: [Character]
+    
+    var formatterWord: String{
+        var guessedWord = ""
+        for letter in word{
+            if guessedLetters.contains(letter){
+                guessedWord += "\(letter)"
+            } else {
+                guessedWord += "_"
+            }
+        }
+        return guessedWord
+    }
+    
+    mutating func playerGuessd(letter: Character){
+        guessedLetters.append(letter)
+        if !word.contains(letter){
+            incorrectMovesRemaining -= 1
+        }
+    }
+}
--- SwiftFundamentals/ViewController.swift
+++ SwiftFundamentals/ViewController.swift
@@ -8,12 +8,84 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var listOfWords = ["buccaneer", "swift", "glorious", "incandescent", "bug", "program"];
+    let incorrectMovesAllowed = 7;
+    
+    var totalWins = 0 {
+        didSet{
+            newRound()
+        }
+    }
+    var totalLosses = 0{
+        didSet{
+            newRound()
+        }
+    }
+    
+    var currentGame: Game!
 
+    @IBOutlet var treeImageView: UIImageView!
+    @IBOutlet var correctWorldLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+
+    @IBOutlet var letterButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        newRound();
     }
 
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        //print(sender.isEnabled)
+        sender.isEnabled = false;
+        let letterString = sender.configuration!.title!;
+        let letter = Character(letterString.lowercased());
+        currentGame.playerGuessd(letter: letter)
+        updateGameState()
+    }
+    
+    
+    func newRound(){
+        if !listOfWords.isEmpty{
+            let newWord = listOfWords.removeFirst();
+            currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: []);
+            enableLetterButtons(true)
+            updateUI()
+        } else {
+            enableLetterButtons(false)
+            updateUI()
+        }
 
+    }
+    
+    func updateUI(){
+        var letters = [String]()
+        for letter in currentGame.formatterWord{
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWorldLabel.text = wordWithSpacing;
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)";
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    }
+    
+    func updateGameState(){
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formatterWord{
+            totalWins += 1
+        } else {
+            updateUI()
+        }
+    }
+    
+    func enableLetterButtons(_ enable: Bool){
+        for button in letterButtons{
+            button.isEnabled = enable
+        }
+    }
+    
 }
 
